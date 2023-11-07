3d7c9fe5f5757d4e48c44d4fbb46b344a84e9844
leevina <144768064+leevina@users.noreply.github.com>
2023-11-07 15:23:10 +0700

    Add files via upload

---
+++ TUGAS/script.js
@@ -0,0 +1,13 @@
+// Algoritma Lulus
+// 1). Siswa input ranking
+// 2). Kalau siswa memiliki ranking diatas 20 maka lulus
+// 3). Kalau siswa memiliki ranking dibawah 20 maka tidak lulus
+
+
+let ranking = prompt("Masukkan ranking")
+
+if (ranking <= 20){
+    alert ("lulus")
+} else {
+    alert (" tidak lulus")
+}
